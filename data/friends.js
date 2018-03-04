9c135a404da1959ad4815be63044fe14a0ea738b
idaselma3025 <idaselma3025@gmail.com>
2018-03-04 15:32:20 -0600

    fixing

---
+++ data/friends.js
@@ -0,0 +1,31 @@
+var friendsArray =[
+  {
+    "name":"Ahmed",
+    "photo":"https://www.gettyimages.com/gi-resources/images/Embed/new/embed2.jpg",
+    "scores":[
+        "5",
+        "2",
+        "4",
+      ]
+  },
+  {
+    "name":"Beth",
+    "photo":"https://www.gettyimages.com/gi-resources/images/Embed/new/embed2.jpg",
+    "scores":[
+        "1",
+        "3",
+        "4",
+      ]
+  },
+  {
+    "name":"Tony",
+    "photo":"https://www.gettyimages.com/gi-resources/images/Embed/new/embed2.jpg",
+    "scores":[
+        "5",
+        "1",
+        "4",
+      ]
+  }
+];
+
+module.exports = friendsArray;
